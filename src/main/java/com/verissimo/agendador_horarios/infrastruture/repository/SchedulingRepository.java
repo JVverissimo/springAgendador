c34459d905832c4237c382dcc9f1572968febcd8
Joao Victor Verissimo <joaovictorverissimo20@gmail.com>
2026-03-19 19:33:15 -0300

    delete_cliente

--- src/main/java/com/verissimo/agendador_horarios/infrastruture/repository/SchedulingRepository.java
+++ src/main/java/com/verissimo/agendador_horarios/infrastruture/repository/SchedulingRepository.java
@@ -1,9 +1,24 @@
 package com.verissimo.agendador_horarios.infrastruture.repository;
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.verissimo.agendador_horarios.infrastruture.entity.SchedulingEntity;
 
+import jakarta.transaction.Transactional;
+
 @Repository
 public interface SchedulingRepository extends JpaRepository<SchedulingEntity, Long> {    
+
+    SchedulingEntity finByServiceSchedulingEntityBetween(String servico, LocalDateTime dataInicio, LocalDateTime datafim);
+
+    @Transactional
+    void deleteBySchedulingEntityClient(LocalDateTime dataHoraAgendamento, String cliente);
+   
+   
+    SchedulingEntity findByDataHoraAgendamentoBetween(LocalDataTime dataHoraInicial, LocalDataTime dataHoraFinal);
+    
+
 }
+
--- src/main/java/com/verissimo/agendador_horarios/service/SchedulingService.java
+++ src/main/java/com/verissimo/agendador_horarios/service/SchedulingService.java
@@ -1,9 +1,14 @@
 package com.verissimo.agendador_horarios.service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Objects;
+
 import org.springframework.stereotype.Service;
+
 import com.verissimo.agendador_horarios.infrastruture.entity.SchedulingEntity;
 import com.verissimo.agendador_horarios.infrastruture.repository.SchedulingRepository;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -13,12 +18,25 @@ public class SchedulingService {
     private final SchedulingRepository schedulingRepository;
 
     public SchedulingEntity saveScheduling(SchedulingEntity entity) {
-        LocalDateTime horaFinal = entity.getDataHoraAgendamento();
-        LocalDateTime horafim = entity.getDataHoraAgendamento().plusHours(1);
+        LocalDateTime horaAgendamento= entity.getDataHoraAgendamento();
+        LocalDateTime horaFim = entity.getDataHoraAgendamento().plusHours(1);
 
-        
+        SchedulingEntity agendados =  schedulingRepository.finByServiceSchedulingEntityBetween(entity.getServico(), horaAgendamento, horaFim);
 
+
+        if (Objects.nonNull(agendados)){
+            throw new RuntimeException("Horário já preenchido");     
+        }
+        schedulingRepository.save(agendados);
 
         return schedulingRepository.save(entity);
+    }
+    public void deleteSchedulingEntity(LocalDateTime datagHoraAgendamento, String client ){
+        schedulingRepository.deleteBySchedulingEntityClient(datagHoraAgendamento, client);
+
+    }
+
+    public SchedulingEntity buscarAgendamento(LocalDate dataHoraInicio){
+        
     }
 }
